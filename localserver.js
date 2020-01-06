b24c25f13a8972adc73f0a6ae116dfe931fb09d4
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-01-06 18:10:15 +0100

    improving server logic

--- localserver.js
+++ localserver.js
@@ -1,15 +1,25 @@
 const http = require('http');
 const ip = '127.0.0.1';
 const port = 4000;
+const fs = require('fs');
 
-const server = http.createServer((request, response)=>{
-    console.log('request starting...');
-
-    //response
-    response.write('Hello Client');
+const send404 = (response)=>{
+    response.WriteHead(404,{'Content-type': 'text/html'});
+    response.write('Error 404: Resource not found!');
     response.end();
-});
+}
 
-server.listen(port, ip,()=>{
+const server = http.createServer((req, res)=>{
+    if(req.method == 'GET' && req.url=='/'){
+        console.log('request starting...');
+        res.writeHead(200,{'Content-type': 'text/html'});
+        fs.createReadStream('./public/index.html').pipe(res);
+    }else{
+        send404(res);
+    }
+    //response
+    // res.write('Hello Client');
+    // res.end();
+}).listen(port, ip,()=>{
     console.log(`Server running at http://${ip}:${port}`);
 })
