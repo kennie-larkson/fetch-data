d7073a95c10033f7f92e8a69f27a552a260c92c2
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-01-08 14:22:51 +0100

    added path module

--- localserver.js
+++ localserver.js
@@ -2,6 +2,7 @@ const http = require('http');
 const ip = '127.0.0.1';
 const port = 4000;
 const fs = require('fs');
+const path = require('path');
 
 const send404 = (response)=>{
     response.writeHead(404,{'Content-type': 'text/html'});
@@ -9,13 +10,43 @@ const send404 = (response)=>{
     response.end();
 }
 
+const mimeLookUp = {
+    '.js': 'application/javascript',
+    '.html': 'text/html'
+}
+
+
+
 const server = http.createServer((req, res)=>{
-    if(req.method == 'GET' && req.url=='/'){
-        console.log(req.method);
-        console.log('request starting...');
-        res.writeHead(200,{'Content-type': 'text/html'});
-        // res.write('Hello Client<br>');
-        fs.createReadStream('./public/index.html').pipe(res);
+    if(req.method == 'GET'){
+
+        //resolve filepath to file system path
+        const fileUrl;
+        if(req.url == '/'){
+            fileUrl = '/index.html';
+            ;
+        }else{  fileUrl = req.url; const filepath = path.resolve('./public'+fileUrl); } 
+        //look up mime type
+        const fileExt = path.extname(filepath);
+        const mimeType = mimeLookUp[fileExt];
+
+        if(!mimeType){
+            send404(res);
+            return
+        }
+        //see if we have that file
+        fs.exists(filepath, (exists)=>{
+            //if not
+            if(!exists){
+                send404(res);
+                return;
+            };
+            //finally stream the file
+            res.writeHead(200,{'Content-type': mimeType});
+            fs.createReadStream(filepath).pipe(res);
+
+        })
+        
     }else{
         send404(res);
     }
